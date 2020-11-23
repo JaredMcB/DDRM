c41f13f7d1a93e6934ffe1a7fa6f4bf889f19dd1
K K Lin <klin@math.arizona.edu>
2020-11-22 17:20:01 -0700

    KLPowerSpec.jl  SDE.jl   : updated to save memory

--- klin/SDE.jl
+++ klin/SDE.jl
@@ -26,22 +26,40 @@ end
 
 ## Generate one sample path.
 function onepath(drift, nsteps, dt;
+                 init = 0.0,
                  sigma=0.0,
                  Stepper = EulerStepper,
                  noise=[],
-                 flags...)
+                 ninit=0,
+                 stride=1,
+                 )
 
-    x       = zeros(nsteps)
-    t       = (0:(nsteps-1))*dt
+    nx = div(nsteps - ninit, stride)
+    x  = zeros(nx)
+    x0 = init
+
     onestep = Stepper(drift, dt; sigma=sigma)
 
+    ## initialize
+    for i=1:ninit
+        x0 = onestep(x0)
+    end
+    x[1] = x0
+    
     if length(noise)>0
-        for n=2:nsteps
+        if stride > 1
+            error("unsupported")
+        end
+
+        for n=2:nx
             x[n] = onestep(x[n-1],noise[n-1])
         end
     else
-        for n=2:nsteps
+        for n=2:nx
             x[n] = onestep(x[n-1])
+            for i=2:stride
+                x[n] = onestep(x[n])
+            end
         end
     end
     return x
