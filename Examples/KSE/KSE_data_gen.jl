32e69dab9f91090257057ed344866aaa958f6de4
Jared McBride <jaredm@math.arizona.edu>
2020-11-24 10:34:05 -0700

    fixed a few things do that it runs on thelio better. I added the gen variable and change default save location.

--- Examples/KSE/KSE_data_gen.jl
+++ Examples/KSE/KSE_data_gen.jl
@@ -14,6 +14,9 @@ using Random
 
 include("Model_KSE.jl")
 
+gen = "lin"     # this is just a reference designation it shows up in the
+                # output file. I think of generatrion.
+
 T        = 10^5 # Length (in seconds) of time of run
 T_disc   = 10^5 ÷ 2 # Length (in seconds) of time discarded
 P        = 2π/sqrt(0.085)  # Period
@@ -27,17 +30,23 @@ Random.seed!(seed)
 uu, vv, tt =  my_KSE_solver(T; T_disc, P, N, h, g, n_gap = obs_gap)
 
 paramaters = Dict(
-   "T" => T,
+   "gen" => gen,
+    "T" => T,
    "T_disc" => T_disc,
    "P" => P,
    "N" => N,
    "h" => h,
    "g" => "x -> cos(π*x/16)*(1 + sin.(π*x/16))",
    "obs_gap" => obs_gap,
+   "seed" => seed,
    "tm" => now()
    )
 
-sol_file = "Examples/KSE/Data/KSE_sol_lin.jld"
+server = startswith(pwd(), "/u5/jaredm") ? true : false
+println("on server = $server")
+sol_file = server ? "../../../data/KSE_Data/KSE_sol_$gen.jld" :
+   "Examples/KSE/Data/KSE_sol_$gen.jld"
+println("Sol save location: " * sol_file)
 
 dat = Dict("dat_uu" => uu, "dat_vv" => vv, "dat_tt" => tt)
 Data = merge(paramaters, dat)
