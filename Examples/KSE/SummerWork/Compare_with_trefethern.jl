408d641ef32393eca624a86d4e11e9fd6c8a7bee
JaredMcB <jaredm@math.arizona.edu>
2020-12-08 16:56:41 -0700

    Added for future reference

---
+++ Examples/KSE/SummerWork/Compare_with_trefethern.jl
@@ -0,0 +1,30 @@
+using PyPlot
+using Random
+using Statistics: mean, var
+
+kse = include("../Model_KSE.jl")
+
+T        = 150 # Length (in seconds) of time of run
+T_disc   = 0    # Length (in seconds) of time discarded
+P        = 32π  # Period
+N        = 128  # Number of fourier modes used
+h        = 1/4  # Timestep
+g        = x -> cos(x/16)*(1 + sin.(x/16))
+obs_gap  = floor(Int, T/h/100)
+seed     = 2021
+
+Δt = h*obs_gap
+uu, vv, tt =  @time kse.my_KSE_solver(T; T_disc, P, N, h, g, n_gap = obs_gap)
+
+t_start = 0
+t_stop = 150
+ind_start = floor(Int,t_start/Δt)+1
+ind_stop =floor(Int,t_stop/Δt)
+H1 = imshow(reverse(uu,dims = 2)', extent=[0,P,0,150], aspect="auto")
+
+mean(uu, dims = 2)
+
+u = rand(128)
+using FFTW
+
+u - ifft(fft(u))
