590b34445b0bccd00128563bfed501e50d10f4b0
JaredMcB <jaredm@math.arizona.edu>
2020-12-11 17:18:14 -0700

    Worked with it alot testing things

--- Examples/KSE/SummerWork/Compare_with_trefethern.jl
+++ Examples/KSE/SummerWork/Compare_with_trefethern.jl
@@ -5,15 +5,16 @@ at = include("../../../Tools/AnalysisToolbox.jl")
 kse = include("../Model_KSE.jl")
 ksed = include("../Model_KSE_Dev.jl")
 
-T        = 1000 # Length (in seconds) of time of run
-T_disc   = T ÷ 2    # Length (in seconds) of time discarded
-P        = 21.55  # Period
-N        = 96  # Number of fourier modes used
-h        = 0.001  # Timestep
+T        = 150 # Length (in seconds) of time of run
+T_disc   = 0    # Length (in seconds) of time discarded
+P        = 32π  # Period
+N        = 128  # Number of fourier modes used
+h        = 1/4  # Timestep
 g        = x -> cos(x/16)*(1 + sin.(x/16))
-obs_gap  = 100
+obs_gap  = floor(Int, T/h/100)
 
 Δt = h*obs_gap
+
 uu, vv, tt    =  @time kse.my_KSE_solver(T; T_disc, P, N, h, g, n_gap = obs_gap)
 uud, vvd, ttd =  @time ksed.my_KSE_solver(T; T_disc, P, N, h, g, n_gap = obs_gap)
 
@@ -24,9 +25,10 @@ t_stop = 150
 ind_start = floor(Int,t_start/Δt)+1
 ind_stop =floor(Int,t_stop/Δt)
 
-H1 = imshow(reverse(uu[:,1:40],dims = 2)', extent=[0,P,0,10], aspect="auto")
 figure()
-H2 = imshow(reverse(uud[:,1:40],dims = 2)', extent=[0,P,0,10], aspect="auto")
+H1 = imshow(reverse(reverse(uu[:,:],dims = 2),dims = 1)', extent=[0,P,0,150], aspect="auto")
+figure()
+H2 = imshow(reverse(reverse(uud[:,:],dims = 2),dims = 1)', extent=[0,P,0,150], aspect="auto")
 
 
 mean(uu, dims = 2)
@@ -52,21 +54,20 @@ function NonLind(v)
     nv[1:N]
 end
 
-Nv = NonLin(v)
+Nv  = NonLin(v)
 Nvd = NonLind(vd)
 
 
-EE = (abs.(vv[:,40:end]).^2)
-EEd =(abs.(vvd[:,40:end]).^2)
+EE  = mean(abs2.(vv),dims = 2)
+EEd = mean(abs2.(vvd),dims = 2)
+EEd_woa = mean(abs2.(vvd),dims = 2)
+
 
 H1 = imshow(reverse(EE,dims = 2)', extent=[0,P,0,10], aspect="auto")
 figure()
 H2 = imshow(reverse(EEd,dims = 2)', extent=[0,P,0,10], aspect="auto")
 
-E = mean(EE,dims = 2)
-Ed = mean(EEd,dims = 2)
-
-semilogy([E 16000*Ed])
+semilogy(EE)
 
 v2 =vv[3,:]
 vd2 =vvd[3,:]
@@ -91,4 +92,6 @@ for i = 2:6
     plot(EE[i,:])
 end
 
+figure()
 semilogy([EE EEd])
+semilogy(EEd_woa)
