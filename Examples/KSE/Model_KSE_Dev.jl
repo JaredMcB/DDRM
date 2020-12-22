60c17f1f90e6826b0eb3c445ee87478995aeddcb
JaredMcB <jaredm@math.arizona.edu>
2020-12-22 14:40:41 -0700

    Reverted to older code to prevent NaNs in solution

--- Examples/KSE/Model_KSE_Dev.jl
+++ Examples/KSE/Model_KSE_Dev.jl
@@ -9,70 +9,66 @@ function my_KSE_solver(
     h :: Real = 1/4, # Timestep
     g = x -> cos(π*x/16)*(1 + sin.(π*x/16)), # Initial condition function
     T_disc = T/2,
-    n_gap = 100, # 1 +  No. of EDTRK4 steps between reported data
-    dealias = false
+    n_gap = 100 # 1 +  No. of EDTRK4 steps between reported data
     )
 
 
+
     ## Spatial grid and initial conditions:
-    x = P*(1:N)/N
+    x = P*(0:2N)/(2N+1)
     u = g.(x)
-    v = ifft(u)                  # ifft to get division by N this definition is better here
+    v = fft(u)
 
     ## Precompute various ETDRK4 scalar quantities:
-    q = 2π/P*[0:N÷2-1; 0; N÷2-N+1:-1]
+    q = fftshift(2π/P*(-N:N))
     L = q.^2 - q.^4
     E = exp.(h*L); E2 = exp.(h/2*L)
 
-    M = 32 # no. of pts use in contour integration
-    r = exp.(im*π*((1:M) .-.5)/M) # roots of unit suggested by Kassam and Trefethen
-    LR = h*L*ones(M)' + ones(N)*r' # the second dim varies r the first vaeries L
+    M  = 32 # no. of pts use in contour integration
+    r  = exp.(im*π*((1:M) .-.5)/M) # roots of unit suggested by Kassam and Trefethen
+    LR = h*L*ones(M)' + ones(2N+1)*r' # the second dim varies r the first vaeries L
 
-    Q = h*real(mean((exp.(LR/2) .- 1)./LR, dims=2))[:]
+    Q  = h*real(mean((exp.(LR/2) .- 1)./LR, dims=2))[:]
     f1 = h*real(mean((-4 .- LR+exp.(LR).*(4 .- 3*LR + LR.^2))./LR.^3,dims=2))[:]
     f2 = h*real(mean((2 .+ LR+exp.(LR).*(-2 .+ LR))./LR.^3,dims=2))[:]
     f3 = h*real(mean((-4 .- 3*LR-LR.^2+exp.(LR).*(4 .- LR))./LR.^3,dims=2))[:]
 
     ## Some declareations
 
-    a  = Complex.(zeros(N))
-    b  = Complex.(zeros(N))
-    c  = Complex.(zeros(N))
-    Nv = Complex.(zeros(N))
-    Na = Complex.(zeros(N))
-    Nb = Complex.(zeros(N))
-    Nc = Complex.(zeros(N))
+    a  = Complex.(zeros(2N+1))
+    b  = Complex.(zeros(2N+1))
+    c  = Complex.(zeros(2N+1))
+    Nv = Complex.(zeros(2N+1))
+    Na = Complex.(zeros(2N+1))
+    Nb = Complex.(zeros(2N+1))
+    Nc = Complex.(zeros(2N+1))
 
     # Main time-stepping loop
     n_max  = round(Int,T/h)
     n_obs  = floor(Int,n_max/n_gap)
     n_disc = floor(Int,T_disc/h/n_gap)
-    ℓ      = 0.5im*q            # julia puts the negative exponent in fft since
-                                # we are using ifft for the scaling the positve
-                                # comes down when the derivative is taken
-    if dealias
-        Nh    = ceil(Int,N/2)
-        v_pad = [v[1:Nh]; zeros(2N); v[Nh+1:end]]
-        F     = plan_ifft(v_pad)        # julia's ifft is my fft for this problem.
-        iF    = plan_fft(v_pad)         # julia's fft is my ifft for this problem.
+    ℓ      = -0.5im*q
 
-        NonLin = function (v)
-            v_pad = v_pad = [v[1:Nh]; zeros(2N); v[Nh+1:end]]
-            nv    = F*(real(iF*v_pad)).^2
-            nv[1:N]
-        end
-    else
-        ## Not correcting for aliasing
-        F = plan_ifft(v)          # julia's ifft is my fft for this problem.
-        iF = plan_fft(v)          # julia's fft is my ifft for this problem.
-        NonLin = v -> F*(real(iF*v)).^2
-    end
+    # v_pad = [v; zeros(2N+1)]
+    # F     = plan_fft(v_pad)
+    # iF    = plan_ifft(v_pad)
+    #
+    # function NonLin(v)
+    #     v_pad = [v; zeros(2N+1)]
+    #     nv    = F*(real(iF*v_pad)).^2
+    #     nv[1:2N+1]
+    # end
 
-    vv = complex(zeros(N, n_obs+1)); vv[:,1]= v
-    uu = zeros(N, n_obs+1); uu[:,1]= u
+    ## Not correcting for aliasing
+    F  = plan_fft(v)
+    iF = plan_ifft(v)
+    NonLin(v) = F*(real(iF*v)).^2
+
+    vv = complex(zeros(2N+1, n_obs+1)); vv[:,1]= v
+    uu = zeros(2N+1, n_obs+1); uu[:,1]= u
     tt = zeros(n_obs+1); tt[1] = 0
     for n = 1:n_max
-        t = n*h
+        t   = n*h
         Nv .= ℓ.* NonLin(v)
         @.  a  =  E2*v + Q*Nv
         Na .= ℓ.* NonLin(a)
@@ -83,14 +79,12 @@ function my_KSE_solver(
         @. v =  E*v + Nv*f1 + 2*(Na+Nb)*f2 + Nc*f3
         if n % n_gap == 0
             ni = Int64(n÷n_gap) + 1
-            u = real.(fft(v))   # julia's fft is my ifft for this problem.
+            u  = real.(ifft(v))
             uu[:,ni] = u
             vv[:,ni] = v
-            tt[ni] = t
+            tt[ni]   = t
         end
     end
-    # Energy spectrum
-    EE = log.(abs.(vv).^2)
 
     start = n_disc+1
     uu[:,start:end], vv[:,start:end], tt[1:end-start+1]
