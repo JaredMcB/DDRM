3c156d6153115133919e8b97d2d478d4208b53c3
JaredMcB <jaredm@math.arizona.edu>
2021-01-26 13:28:16 -0700

    Altered to make Jan 25 plot. This is where I ran ks.jl with my nonlinear function in it.

--- klin/RuningLinSolver.jl
+++ klin/RuningLinSolver.jl
@@ -10,15 +10,34 @@ steps = 1000
 N = 128
 
 # Trefethen
-vv = ks.run_ks(steps,1.5;
+vv_myA = ks.run_ks(steps,1.5;
              nsubsteps = 6,
              verbose = false, L = P)
 
 # get solution
-uu = real(N*ifft([zeros(1, steps+1); vv; reverse(conj(vv),dims = 1)],1))
+# uu = real(N*ifft([zeros(1, steps+1); vv; reverse(conj(vv),dims = 1)],1))
+# H1 = imshow(reverse(reverse(uu',dims = 1),dims = 2), extent=[0,P,0,150], aspect="auto")
 
-H1 = imshow(reverse(reverse(uu',dims = 1),dims = 2), extent=[0,P,0,150], aspect="auto")
-uu
+
+VV = [vv_lin, vv_myNA, vv_myA]
+UU = [uu_my,uu_lin, uu]
+
+sty = ["-","--",":"]
+labels = ["vv_lin", "vv_myNA", "vv_myA"]
+for i = 1:5
+    for j = 1 : length(VV)
+        subplot(5,1,i)
+        plot(1.5*(1:300),real(VV[j][i,1:300]),linestyle = sty[j])
+    end
+    title("model $i")
+end
+suptitle("Comparison of L's nonlinear part (solid) with M's nonlinear part (dealiased, dashed;aliased, dotted) in L's ODE solver")
+
+
+
+
+
+
 v = ks.kt_init()
 
 vc = real(N*ifft([0; v[:];0;reverse(conj(v[:]))]))
