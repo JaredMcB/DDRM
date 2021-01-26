d1b93d9b860cefaa46fa7d080f3020b0e6227379
JaredMcB <jaredm@math.arizona.edu>
2021-01-26 13:27:16 -0700

    Added a few things, I implemented both functions my self and add one more test.

--- klin/compare-2021-01-21.jl
+++ klin/compare-2021-01-21.jl
@@ -1,4 +1,7 @@
-using DSP,FFTW,ks
+using DSP,FFTW
+
+ks = include("ks.jl")
+
 
 function jaredm(P :: Real = 32π,    # Period
                 n :: Int64 = 64,    # Number of linearly independent fourier modes used (beyond 0)
@@ -34,13 +37,13 @@ end
 function klin(P :: Real = 32π,    # Period
               n :: Int64 = 64,    # Number of linearly independent fourier modes used (beyond 0)
               )
-    
-    ks! = make_ks_field(n; L=P,alpha=0,beta=0)
-    
+
+    ks! = ks.make_ks_field(n; L=P,alpha=0,beta=0)
+
     function(v)
         ## input is a vector of Fourier coefficents v[k] for
         ## k>0; we assume v[-k] = conj(v[k]) and v[0]=0.
-        
+
         ## if input is too short, pad it
         if length(v) < n
             v = vcat(v,zeros(n-length(v)))
@@ -109,3 +112,67 @@ end
 #  -1.2224603334267127e-14 - 251.99999999999994im
 #   -7.646126771277965e-15 - 91.00000000000003im
 #    8.329609062070828e-15 - 64.00000000000003im
+
+
+## Set up
+
+P = 32π
+n = 64
+g = x -> cos(x/16)*(1 + sin(x/16))
+N = 2n+1
+
+# Spatial grid and initial conditions:
+x = P*(0:N-1)/N
+u = g.(x)
+v = fft(u)/N        # The division by N is to effect the DFT I want.
+
+# Now we set up the equations
+q = 2π/P*[0:n; -n:-1]
+ℓ = -0.5im*q
+
+Diff(x,y) = maximum(abs.(x - y))
+
+## Dr. Lins function
+fillout(v::Array{Complex{Float64},1}) = [0; v; reverse(conj(v))]
+
+NonLin! = ks.make_ks_field(n; alpha = 0, beta = 0, L = P)
+
+NonLin = function (v)
+    u = zeros(Complex{Float64},n)
+    NonLin!(v[2:n+1],u)
+    fillout(u)
+end
+
+## My function
+pad = n
+K = 2n+1 + pad
+NonLinNA = function (v)
+    v_pad = [0;v[2:n+1]; zeros(pad);v[n+2:N]]
+    nv = fft(bfft(v_pad).^2)/K
+    Nv_dealiased = ℓ .* [nv[1:n+1]; nv[end-n+1:end]]
+end
+
+# N(v) truth
+A       = zeros(ComplexF64,n)
+a       = [-.25im, .25, -.25im,-.0625]
+A[1:4]  = a
+A       = fillout(A)
+A       .*= ℓ
+
+# julia> Diff(A,klin()(v[2:65]))
+# 1.6553884238885772e-16
+#
+# julia> Diff(A,jaredm()(v[2:65]))
+# 1.649639905177841e-16
+#
+# julia> Diff(jaredm()(v[2:65]),klin()(v[2:65]))
+# 1.7542365736134082e-16
+#
+# julia> Diff(klin()(vv[2:65]),NonLin(vv))
+# 0.0
+#
+# julia> Diff(jaredm()(vv[2:65]),NonLinNA(vv))
+# 2.282727361246348e-16
+#
+# julia> Diff(jaredm()(vv[2:65]),klin()(vv[2:65]))
+# 3.2191158424353993e-16
