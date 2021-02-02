8f8bb5aa1bdc30361610db874a2cabd25f76bcfc
JaredMcB <jaredm@math.arizona.edu>
2021-02-02 12:56:33 -0700

    Made to generate and save data from Dr. Lin's solver.

--- klin/RuningLinSolver.jl
+++ klin/RuningLinSolver.jl
@@ -1,3 +1,64 @@
+################################################################################
+#
+# File: RunningLinSolver.jl
+# Author: Jared McBride (Feb-2-2021)
+#
+# This just generates and saves KSE model runs using
+# Dr. Lin's solver.
+#
+################################################################################
+
+using JLD
+using Dates
+
+ks = include("ks.jl")
+
+gen = "lin"     # this is just a reference designation it shows up in the
+                # output file. I think of generatrion.
+
+T        = 10^5 # Length (in seconds) of time of run
+T_disc   = 10^5 ÷ 2 # Length (in seconds) of time discarded (taken from T)
+P        = 2π/sqrt(0.085)  # Period
+n        = 96  # Number of fourier modes used
+h        = 1e-3 # Timestep
+g        = x -> cos(π*x/16)*(1 + sin.(π*x/16))
+obs_gap  = 100
+
+
+steps   = ceil(Int,T/h)
+discard = ceil(Int,T_disc/h)
+
+vv = ks.run_ks(steps, .1;
+             nsubsteps = obs_gap,
+             verbose = false, L = P)
+
+paramaters = Dict(
+   "gen" => gen,
+    "T" => T,
+   "T_disc" => T_disc,
+   "P" => P,
+   "N" => N,
+   "h" => h,
+   "g" => "x -> cos(π*x/16)*(1 + sin.(π*x/16))",
+   "obs_gap" => obs_gap,
+   "seed" => seed,
+   "tm" => now()
+   )
+
+server = startswith(pwd(), "/u5/jaredm") ? true : false
+println("on server = $server")
+sol_file = server ? "../../../data/KSE_Data/KSE_sol_$gen.jld" :
+   "Desktop/DDMR/Examples/KSE/Data/KSE_sol_$gen.jld"
+println("Sol save location: " * sol_file)
+
+dat = Dict("dat_uu" => uu, "dat_vv" => vv, "dat_tt" => tt)
+Data = merge(paramaters, dat)
+save(sol_file, Data)
+println("data saved")
+
+
+
+
 using PyPlot
 using FFTW
 
