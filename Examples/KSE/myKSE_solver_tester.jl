a7e71e26377120b54c51f27f73ecf3e47c4b8cc7
JaredMcB <jaredm@math.arizona.edu>
2021-02-02 12:55:57 -0700

    Cleaned it up a bit.

--- Examples/KSE/myKSE_solver_tester.jl
+++ Examples/KSE/myKSE_solver_tester.jl
@@ -22,7 +22,7 @@ vv = mykse.my_KSE_solver(T; P, n, h, g, T_disc, n_gap)
 
 uu = real((2n+1)*ifft(vv,1))
 figure()
-H1 = imshow(reverse(reverse(uu[:,:]',dims=2),dims = 1), extent=[0,P,0,150], aspect="auto")
+H1 = imshow(reverse(reverse(uu[:,1:100]',dims=2),dims = 1), extent=[0,P,0,150], aspect="auto")
 
 # Dr. Lin's
 steps = ceil(Int,T/h/n_gap) - 1
@@ -34,6 +34,8 @@ vv_k = ks.run_ks(ks.kt_init(;n=129),steps,1.5;
 uu_k = real((2n+1)*ifft([zeros(1, steps+1); vv_k; reverse(conj(vv_k),dims = 1)],1))
 H1 = imshow(reverse(reverse(uu_k[:,1:100]',dims = 1),dims = 2), extent=[0,P,0,150], aspect="auto")
 
+
+test(vv,vv_k)
 ## Plot trajectories
 function test(vv,vv_k)
     figure()
@@ -41,36 +43,37 @@ function test(vv,vv_k)
     for i in modes
         subplot(length(modes),1,i)
         plot(real(vv[i+1,:]),label ="mine")
-        plot(real(vv_k[i,:]),":",label ="klin")
+        plot(real(vv_k[i+1,:]),":",label ="klin")
         title("mode $i")
         legend()
     end
 
     # Autocorellations
-    lags = -100:100
+    lags = -1000:1000
     figure()
     for i in modes
         subplot(length(modes),1,i)
         A = at.my_autocor(real(vv[i+1,:]),lags)
-        plot(1.5*lags,A,label ="mine")
-        A_k = at.my_autocor(real(vv_k[i,:]),lags)
-        plot(1.5*lags,A_k,":",label ="klin")
+        plot(.1*lags,A,label ="mine")
+        A_k = at.my_autocor(real(vv_k[i+1,:]),lags)
+        plot(.1*lags,A_k,":",label ="klin")
         title("mode $i")
         legend()
     end
 
     # Energy spectrum
     figure()
-    E = mean(abs2.(vv[2:65,:]),dims = 2)[:]
+    E = mean(abs2.(vv),dims = 2)[:]
     semilogy(E,label ="mine")
     E_k = mean(abs2.(vv_k),dims = 2)[:]
     semilogy(E_k,label ="klin")
+    grid()
     legend()
 end
 
 ## 2017 parameters
-T        = 10^4     # Length (in seconds) of time of run
-T_disc   = T÷ 2     # Length (in seconds) of time discarded
+T        = 10^3     # Length (in seconds) of time of run
+T_disc   = 0 #T ÷ 2     # Length (in seconds) of time discarded
 P        = 2π/sqrt(0.085)  # Period
 n        = 96       # Number of fourier modes used
 h        = .001     # Timestep
@@ -100,68 +103,4 @@ figure()
 H1 = imshow(reverse(reverse(uu_k[:,end-1000+1 : end]',dims = 1),dims = 2), extent=[0,P,0,150], aspect="auto")
 
 ## Plot trajectories
-test(vv[:,end-50000+1 : end], vv_k[:,end-50000+1 : end])
-
-## Energy Spectrum
-
-V = [vv[:,end - 50000 +1 : end],vv_k[:,end - 50000 +1 : end]]
-
-using Statistics: mean
-for v in V
-    E = mean(abs2.(v)[1:30],dims = 2)
-    plot(E)
-end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# My New Solver
-vv = mykse.my_KSE_solver(T;
-                        P,
-                        n,
-                        h,
-                        g,
-                        T_disc,
-                        n_gap,
-                        aliasing)``
-
-uu = real((2n+1)*ifft(vv,1))
-ender = findfirst(isnan, sum(uu,dims = 1))[2]-1000
-H1 = imshow(reverse(uu[:,1:ender]',dims = 1), extent=[0,P,0,1000], aspect="auto")
-
-EE = abs2.(vv)
-figure()
-semilogy(mean(EE[:,1:ender],dims=2),label = "me (Julia) $i")
-
-lags = -50:50
-A = at.my_autocor(vv[3,1:ender],lags)
-plot(A)
-# MyOld Solver
-uu_o, vv_o, tt = kse.my_KSE_solver(T;
-                        P,
-                        N = 2n+1,
-                        h,
-                        g,
-                        T_disc,
-                        n_gap,
-                        aliasing)
-
-ender = findfirst(isnan, sum(uu_o,dims = 1))[2]-10
-figure()
-H1 = imshow(reverse(uu_o[:,1:ender]',dims = 1), extent=[0,P,0,150], aspect="auto")
-
-EE_o = abs2.(vv_o)
-semilogy(mean(EE_o[:,1:ender],dims=2),label = "me (Julia) $i")
-
-A_o = at.my_autocor(vv_o[3,1:ender],lags)
-plot(A_o)
+test(vv[:,end-5000+1 : end], vv_k[:,end-5000+1 : end])
