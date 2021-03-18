6cff9092d36f22f85b50c63ae5291305967556fe
JaredMcB <jaredm@math.arizona.edu>
2021-03-18 16:44:27 -0700

    Added loop to make 5 data sets, with random initial conditions.

--- Examples/KSE/KSE_data_gen.jl
+++ Examples/KSE/KSE_data_gen.jl
@@ -13,16 +13,21 @@ using Dates
 
 kse = include("myKSE_solver.jl")
 
-gen = "lin1e3"     # this is just a reference designation it shows up in the
+gen = "lin1e5"     # this is just a reference designation it shows up in the
                 # output file. I think of generatrion.
 
-T        = 10^3 # Length (in seconds) of time of run
+T        = 10^5 # Length (in seconds) of time of run
 T_disc   = T ÷ 2 # Length (in seconds) of time discarded
 P        = 2π/sqrt(0.085)  # Period
 n        = 96  # Number of fourier modes used
 h        = 1e-3 # Timestep
-g        = x -> cos(π*x/16)*(1 + sin.(π*x/16))
 obs_gap  = 100
+
+for i = 1:5
+
+gen = "lin1e5_r$i"
+r = randn(6)
+g(x) = sum(r[n+1]*cos(2π/P*n*x) for n = 0:5)
 
 vv = @time kse.my_KSE_solver(T; T_disc, P, n, h, g, n_gap = obs_gap)
 
@@ -33,7 +38,8 @@ paramaters = Dict(
    "P" => P,
    "n" => n,
    "h" => h,
-   "g" => "x -> cos(π*x/16)*(1 + sin.(π*x/16))",
+   "g" => "x -> sum(r[n+1]*cos(2π/P*n*x) for n = 0:5)",
+   "r" => r,
    "obs_gap" => obs_gap,
    "tm" => now()
    )
@@ -48,4 +54,6 @@ println("Sol save location: " * sol_file)
 dat = Dict("dat_vv" => vv)
 Data = merge(paramaters, dat)
 save(sol_file, Data)
-println("data saved")
+println("data $i saved")
+
+end
