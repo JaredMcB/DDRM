b3da02b66b5132767578dce5a20f82f93112b009
JaredMcB <jaredm@math.arizona.edu>
2021-03-23 07:53:55 -0700

    Preparing I have tested these scripts on laptopp and now will run them on thelio.

--- Examples/KSE/KSE_data_gen.jl
+++ Examples/KSE/KSE_data_gen.jl
@@ -23,37 +23,37 @@ n        = 96  # Number of fourier modes used
 h        = 1e-3 # Timestep
 obs_gap  = 100
 
-for i = 1:5
+for i = 1:2
 
-gen = "lin1e5_r$i"
-r = randn(6)
-g(x) = sum(r[n+1]*cos(2π/P*n*x) for n = 0:5)
+   gen = "lin1e5_r$i"
+   r = randn(6)
+   g(x) = sum(r[n+1]*cos(2π/P*n*x) for n = 0:5)
 
-vv = @time kse.my_KSE_solver(T; T_disc, P, n, h, g, n_gap = obs_gap)
+   vv = @timev kse.my_KSE_solver(T; T_disc, P, n, h, g, n_gap = obs_gap)
 
-paramaters = Dict(
-   "gen" => gen,
-    "T" => T,
-   "T_disc" => T_disc,
-   "P" => P,
-   "n" => n,
-   "h" => h,
-   "g" => "x -> sum(r[n+1]*cos(2π/P*n*x) for n = 0:5)",
-   "r" => r,
-   "obs_gap" => obs_gap,
-   "tm" => now()
-   )
+   paramaters = Dict(
+      "gen" => gen,
+       "T" => T,
+      "T_disc" => T_disc,
+      "P" => P,
+      "n" => n,
+      "h" => h,
+      "g" => "x -> sum(r[n+1]*cos(2π/P*n*x) for n = 0:5)",
+      "r" => r,
+      "obs_gap" => obs_gap,
+      "tm" => now()
+      )
 
-server = startswith(pwd(), "/u5/jaredm") ? true : false
-println("on server = $server")
-sol_file = server ? "../../../data/KSE_Data/ks_sol_$gen.jld" :
-   "C:/Users/JaredMcBride/Desktop/DDMR/Examples/KSE/Data/ks_sol_$gen.jld"
+   server = startswith(pwd(), "/u5/jaredm") ? true : false
+   println("on server = $server")
+   sol_file = server ? "../../../data/KSE_Data/ks_sol_$gen.jld" :
+      "C:/Users/JaredMcBride/Desktop/DDMR/Examples/KSE/Data/ks_sol_$gen.jld"
 
-println("Sol save location: " * sol_file)
+   println("Sol save location: " * sol_file)
 
-dat = Dict("dat_vv" => vv)
-Data = merge(paramaters, dat)
-save(sol_file, Data)
-println("data $i saved")
+   dat = Dict("dat_vv" => vv)
+   Data = merge(paramaters, dat)
+   save(sol_file, Data)
+   println("data $i saved")
 
 end
