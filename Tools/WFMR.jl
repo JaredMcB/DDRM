5944709d4644a4c2d650c4328d44f363144152c6
Jared McBride <jaredm@math.arizona.edu>
2021-06-08 09:04:11 -0700

    updated matrix_autocov_seq and changed it for at.my_autocov

--- Tools/WFMR.jl
+++ Tools/WFMR.jl
@@ -174,57 +174,6 @@ function spectfact_matrix_CKMS(P; ϵ = 1e-10,
 end
 
 
-function matrix_autocov_seq(pred;
-    L = min(size(pred,2)-1,1500),
-    steps = size(pred,2),
-    nu = size(pred,1),
-    win = "Par"
-    )
-
-    lags = -L:L
-
-    # Smoothed viewing window
-    lam = at._window(L, win = win, two_sided = false)
-
-    R_pred = zeros(ComplexF64,nu,nu,length(-L:L))
-    for i = 1 : nu
-        for j = 1 : nu 
-            @views R_pred[i,j,:] = at.my_crosscov(pred[i,1:steps],pred[j,1:steps],lags)
-        end
-    end
-    for l = 0:L
-        R_pred[:,:,L+1+l] .+= (@view R_pred[:,:,L+1-l])'
-        R_pred[:,:,L+1+l] .*= lam[l+1]/2
-    end
-    R_pred
-end
-
-function my_autocov_alt(pred::Array{<:Number,2};
-    L = min(size(pred,2)-1,1500),
-    steps = size(pred,2),
-    nu = size(pred,1),
-    win = "Par"
-    )
-
-    lags = -L:L
-
-    # Smoothed viewing window
-    lam = at._window(L, win = win, two_sided = false)
-
-    R_pred = zeros(ComplexF64,nu,nu,length(0:L))
-    for i = 1 : nu
-        for j = 1 : nu ## This enforcement of conjugate semetry is very bad. Because
-                       ## cross-spectra are not conjugate symetric. 
-            @views temp = at.my_crosscov(pred[i,1:steps],pred[j,1:steps],lags)
-            @views temp = .5*(temp[L+1:2L+1] + conj!(temp[L+1:-1:1]))
-            temp[1] = real(temp[1])
-            R_pred[i,j,:] = lam .* temp
-        end
-    end
-    R_pred
-end
-
-
 """
     vector_wiener_filter_fft
 
@@ -255,7 +204,7 @@ function vector_wiener_filter_fft(
     nffth = nfft ÷ 2
     L = min(par,steps-1)
 
-    R_pred_smoothed = @timed matrix_autocov_seq(pred; L, steps, nu, win)
+    R_pred_smoothed = @timed at.my_autocov(pred; L, steps, nu, win)
     if verb
         println("Time taken for autocov: ", R_pred_smoothed.time)
         println("Bytes Allocated: ", R_pred_smoothed.bytes)
